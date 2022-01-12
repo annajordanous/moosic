342186fc9e7f110471e3520520c07c6f12806368
Anna Jordanous <annajordanous@hotmail.com>
2022-01-12 13:43:54 +0000

    Fixing typos. Source code from https://pymoo.org/getting_started/source_code.html

--- moosic.py
+++ moosic.py
@@ -1,25 +1,25 @@
 import numpy as np
-import pymoo 
 
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.core.problem import ElementwiseProblem
 from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
 
-class MyProblem(ElementwiseProblem): 
+
+class MyProblem(ElementwiseProblem):
 
     def __init__(self):
         super().__init__(n_var=2,
                          n_obj=2,
                          n_constr=2,
                          xl=np.array([-2, -2]),
-                         xu=np.array([2,2]))
+                         xu=np.array([2, 2]))
 
-    def _evaluate(self, x, out, *args, **kwargs):
+    def _evaluate(self, x, out, *args,  **kwargs):
         f1 = 100 * (x[0] ** 2 + x[1] ** 2)
-        f2 = (x[0] -1) ** 2 + x[1] ** 2
+        f2 = (x[0] - 1) ** 2 + x[1] ** 2
 
-        g1 = 2 * (x[0]) - 0.1) * (x[0] - 0.9) / 0.18
+        g1 = 2 * (x[0] - 0.1) * (x[0] - 0.9) / 0.18
         g2 = - 20 * (x[90] - 0.4) * (x[0] - 0.6) / 4.8
 
         out["F"] = [f1, f2]
@@ -32,7 +32,7 @@ algorithm = NSGA2(pop_size=100)
 
 res = minimize(problem,
                algorithm,
-               ("n_gen", 100)
+               ("n_gen", 100),
                verbose=False,
                seed=1)
 
